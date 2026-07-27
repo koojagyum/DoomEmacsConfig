c7c670ef280e4b1198384aa029cf532eb87a622c
Jagyum Koo <koojagyum@gmail.com>
2026-07-28 01:48:23 +0900

    Dealing with org mode hang issue

--- koodev/+org.el
+++ koodev/+org.el
@@ -1,18 +1,22 @@
 ;;; koodev/+org.el -*- lexical-binding: t; -*-
 
-;; Personal kanban-ish workflow: dropped Doom's PROJ/LOOP/STRT/IDEA/KILL
-;; and added DELEGATED/FAIL on the closed side.
 (after! org
+  ;; Personal kanban-ish workflow: dropped Doom's PROJ/LOOP/STRT/IDEA/KILL
+  ;; and added DELEGATED/FAIL on the closed side.
   (setq org-startup-folded 'fold
         org-todo-keywords
         '((sequence "TODO(t)" "WAIT(w)" "HOLD(h)"
-                    "|" "DONE(d)" "DELEGATED(e)" "CANCEL(c)" "FAIL(f)"))))
+           "|" "DONE(d)" "DELEGATED(e)" "CANCEL(c)" "FAIL(f)"))
+        org-agenda-files '())
+  ;; org mode hang issue
+  (setq org-element-use-cache nil))
 
-(use-package! org-bullets
-  :hook (org-mode . org-bullets-mode))
+;; turn off ws-buster in org buffer
+(after! ws-butler
+  (add-to-list 'ws-butler-global-exempt-modes 'org-mode))
 
 (use-package! org-modern
-  :hook (org-mode . org-modern-mode)
+  :after org
   :config
   (setq org-modern-star 'replace)
   (global-org-modern-mode))
